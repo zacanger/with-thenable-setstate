f208caf7532bee7376b5e51ae55f8e62844fd696
zacanger <zac@zacanger.com>
2018-07-20 10:57:05 -0600

    Add tests

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const withThenableSetState = (Comp) =>
   class extends Comp {
-    thenableSetState(state, cb) {
+    thenableSetState (state, cb) {
       return new Promise((resolve) => {
         this.setState(state, (...args) => {
           if (typeof cb === 'function') {
             cb(args)
           }
+          resolve(args)
         })
       })
     }
---
+++ index.test.js
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import enzyme from 'enzyme'
+import tape from 'tape'
+import HOC from '.'
+
+enzyme.configure({ adapter: new Adapter() })
+
+tape.test('withThenableSetState', (t) => {
+  class Foo extends React.Component {
+    constructor (props) {
+      super(props)
+      this.state = { a: 0 }
+    }
+    thing () {
+      this.thenableSetState(({ a: 1 }))
+        .then(() => {})
+        .catch(() => {})
+    }
+    render () {
+      return <span />
+    }
+  }
+
+  const Bar = HOC(Foo)
+
+  const wrapper = enzyme.shallow(<Bar />)
+  wrapper.instance().thing()
+
+  t.equals(wrapper.state().a, 1)
+  t.end()
+})
