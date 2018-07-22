95507dc933afc37e0a1af9429a6dde40523bc601
zacanger <zac@zacanger.com>
2018-07-22 15:53:32 -0600

    Remove unused cb

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const withThenableSetState = (Comp) =>
   class extends Comp {
-    thenableSetState (state, cb) {
+    thenableSetState (state) {
       return new Promise((resolve) => {
         this.setState(state, (...args) => {
-          if (typeof cb === 'function') {
-            cb(args)
-          }
           resolve(args)
         })
       })
